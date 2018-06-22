2af0dec1b574ccffddb6b09360a113c20dbb73f4
xiwei <xiweizhou123@hotmail.com>
2018-06-22 15:39:37 +0200

    changes

--- finalProject/src/controllers/ProfileController.java
+++ finalProject/src/controllers/ProfileController.java
@@ -45,8 +45,6 @@ public class ProfileController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String profilename = (String)request.getParameter("content");
-		String type = (String)request.getParameter("type");
 		BeanUser user = new BeanUser();
 		HttpSession session = request.getSession();
 		String oldusername = (String) session.getAttribute("user");
